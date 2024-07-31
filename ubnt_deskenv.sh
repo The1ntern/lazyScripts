fdb8b2d5d4c92a35d9d303da7c6d9aeb7d0a49b1
The1ntern <31868760+The1ntern@users.noreply.github.com>
2024-07-30 18:59:28 -0700

    Add Ubuntu Desktop configuration with TightVNC

---
+++ ubnt_deskenv.sh
@@ -0,0 +1,11 @@
+# Install Ubuntu Desktop
+sudo apt update -y 
+sudo apt upgrade -y 
+sudo apt dist-upgrade -y 
+sudo apt autoremove -y 
+sudo apt autoclean -y
+sudo apt install slim -y
+sudo apt install ubuntu-desktop -y
+sudo apt install gnome-panel gnome-settings-daemon metacity nautilus gnome-terminal -y
+sudo apt install tightvncserver -y
+sudo reboot --
