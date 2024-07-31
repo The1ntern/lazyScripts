560497a714372480a9a3f0d0883bc0ec2110a11c
The1ntern <31868760+The1ntern@users.noreply.github.com>
2024-07-30 19:23:17 -0700

    Modified order of commands

--- ubnt_deskenv.sh
+++ ubnt_deskenv.sh
@@ -2,10 +2,10 @@
 sudo apt update -y 
 sudo apt upgrade -y 
 sudo apt dist-upgrade -y 
-sudo apt autoremove -y 
-sudo apt autoclean -y
 sudo apt install slim -y
 sudo apt install ubuntu-desktop -y
 sudo apt install gnome-panel gnome-settings-daemon metacity nautilus gnome-terminal -y
 sudo apt install tightvncserver -y
+sudo apt autoremove -y 
+sudo apt autoclean -y
 sudo reboot --
